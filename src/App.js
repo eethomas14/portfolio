43e627345758f074843f0361d9bf5f024f23f575
eethomas14 <ethomas14@luc.edu>
2021-03-28 13:03:44 -0500

    completed header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from "./components/NavBar";
+import Navbar from './components/NavBar';
+import Header from "./components/Header";
+import Particles from 'react-particles-js';
+
 
 function App() {
   return (
-    <Navbar/>
+      <>
+        <Particles
+            className="particles-canvas"
+            params={{
+              particles: {
+                number: {
+                  value: 30,
+                  density: {
+                    enable: true,
+                    value_area: 900
+                  }
+                },
+                shape: {
+                  type: "",
+                  stroke: {
+                    width: 6,
+                    color: "#f9ab00"
+                  }
+                }
+              }
+
+            }}
+        />
+        <Navbar/>
+        <Header/>
+      </>
   );
-}
+};
 
 export default App;
