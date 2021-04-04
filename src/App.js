3353d0f7775e6e3b66e0c12d7a42091440d52f27
eethomas14 <ethomas14@luc.edu>
2021-04-04 08:30:46 -0500

    added more work to the about me page

--- src/App.js
+++ src/App.js
@@ -8,29 +8,7 @@ import AboutMe from './components/AboutMe';
 
 function App() {
   return (
-      <>
-        <Particles
-            className="particles-canvas"
-            params={{
-              particles: {
-                number: {
-                  value: 30,
-                  density: {
-                    enable: true,
-                    value_area: 900
-                  }
-                },
-                shape: {
-                  type: "",
-                  stroke: {
-                    width: 6,
-                    color: "#f9ab00"
-                  }
-                }
-              }
-
-            }}
-        />
+     <>
         <Navbar/>
         <Header/>
         <AboutMe/>
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,23 +6,20 @@ const AboutMe = () => {
         <div className="container py-5">
             <div className="row">
                 <div className="col-lg-6 col-xm-12">
-                    <div className="photo-wrap">
+                    <div className="photo-wrap mb-5">
                     <img className="profile-img" src={author} alt="author..."/>
                     </div>
                 </div>
                 <div className="col-lg-6 col-xm-12">
-                <h1>About Me</h1>
+                <h1 className="about-heading">About Me</h1>
                     <p>
-                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-                        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                        sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-                        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
-                        laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui
-                        in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat
-                        quo voluptas nulla pariatur?"
+                        "Hi, I'm Emmanuel! I am a graduate student in Computer Science at Loyola University Chicago.
+                        I am a full-stack developer graduating in May 2021. Over the past couple of years I have
+                        focused on Java Spring applications as well as LAMP, MERN and MEAN stacks. I am passionate
+                        about all things tech and I love to learn new technologies and improve my skills. I aspire to
+                        join an organization that values continued learning as much as I do, values diversity in all its
+                        forms, and provides services that make the world a better place."
+
                     </p>
                 </div>
             </div>
